b550f0b0103c7a391ae4db40ede75098ea20e49d
Anna Malorodova <angervenus6@gmail.com>
2021-05-09 20:28:24 +0300

    Update

---
+++ src/components/Datepicker.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+  <v-app id="inspire">
+    <v-row justify="center">
+      <v-date-picker 
+      v-model="date"
+      color="#82B1FF"
+      header-color="#263238"
+      ></v-date-picker>
+    </v-row>
+     {{date}}
+  </v-app>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'Datepicker',
+
+    data(){
+   return{
+    date: new Date().toISOString().substr(0, 10),
+    newDate:"",
+}
+    },
+
+    methods:{
+    
+     
+    }
+
+   
+}
+</script>
