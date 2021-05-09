822935e5a57fb08be177f65a88c0d20d27fa4419
Anna Malorodova <angervenus6@gmail.com>
2021-05-09 20:15:50 +0300

    Inicial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Datepicker/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Datepicker from './components/Datepicker';
+
+export default {
+  name: 'App',
+
+  components: {
+    Datepicker,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
